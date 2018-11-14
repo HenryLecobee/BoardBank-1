ca3b0bd26f8b7c3692005a3021d902242150ed4c
Henry Lo <henryl@ecobee.com>
2018-11-14 11:58:48 -0500

    test 2

--- BoardBankUITests/AddPlayerTests.swift
+++ BoardBankUITests/AddPlayerTests.swift
@@ -53,59 +53,59 @@ class AddPlayerTests: XCTestCase {
     
     // MARK: Tests
     
-    func testAddPlayerWithDefaultBalance() {
-        /**
-         Add new Player with default balance and assert that added player is displayed on the Home Screen
-         */
-        homeScreen.addPlayer()
-        addPlayerScreen.setPlayerNameWith(testPlayerName1)
-        addPlayerScreen.addPlayer()
-        
-        XCTAssertTrue(homeScreen.isPlayerDisplayed(name: testPlayerName1, balance: defaultPlayerBalance, currency: defaultCurrency))
-    }
-    
-    func testAddPlayerWithDefaultBalanceThenDelete() {
-        /**
-         Add new Player with default balance and assert that added player is displayed on the Home Screen
-         Then delete the added player and assert that deleted player is not displayed on the Home Screen
-         */
-        self.testAddPlayerWithDefaultBalance()
-        homeScreen.deletePlayerWith(name: testPlayerName1, balance: defaultPlayerBalance, currency: defaultCurrency)
-        
-        XCTAssertFalse(homeScreen.isPlayerDisplayed(name: testPlayerName1, balance: defaultPlayerBalance, currency: defaultCurrency))
-    }
-    
-    func testAddTwoPlayerWithCustomBalance() {
-        /**
-         Add two new Players with custom balance and assert that added players are displayed on the Home Screen
-         */
-        homeScreen.addPlayer()
-        addPlayerScreen.setPlayerNameWith(testPlayerName1)
-        addPlayerScreen.setPlayerBalanceWith(testPlayerBalance1)
-        addPlayerScreen.addPlayer()
-        
-        homeScreen.addPlayer()
-        addPlayerScreen.setPlayerNameWith(testPlayerName2)
-        addPlayerScreen.setPlayerBalanceWith(testPlayerBalance2)
-        addPlayerScreen.addPlayer()
-        
-        XCTAssertTrue(homeScreen.isPlayerDisplayed(name: testPlayerName1, balance: testPlayerBalance1, currency: defaultCurrency))
-        XCTAssertTrue(homeScreen.isPlayerDisplayed(name: testPlayerName2, balance: testPlayerBalance2, currency: defaultCurrency))
-    }
-    
-    func testAddPlayerWithCustomBalanceThenRename() {
-        /**
-         Add new Player with custom balance and rename then assert that modified player is displayed on the Home Screen
-         */
-        homeScreen.addPlayer()
-        addPlayerScreen.setPlayerNameWith(testPlayerName1)
-        addPlayerScreen.setPlayerBalanceWith(testPlayerBalance1)
-        addPlayerScreen.addPlayer()
-        homeScreen.renamePlayerNameWith(name: testPlayerName1, balance: testPlayerBalance1, currency: defaultCurrency, newName: testPlayerName2)
-        
-        XCTAssertTrue(homeScreen.isPlayerDisplayed(name: testPlayerName2, balance: testPlayerBalance1, currency: defaultCurrency))
-    }
-    
+//    func testAddPlayerWithDefaultBalance() {
+//        /**
+//         Add new Player with default balance and assert that added player is displayed on the Home Screen
+//         */
+//        homeScreen.addPlayer()
+//        addPlayerScreen.setPlayerNameWith(testPlayerName1)
+//        addPlayerScreen.addPlayer()
+//
+//        XCTAssertTrue(homeScreen.isPlayerDisplayed(name: testPlayerName1, balance: defaultPlayerBalance, currency: defaultCurrency))
+//    }
+//
+//    func testAddPlayerWithDefaultBalanceThenDelete() {
+//        /**
+//         Add new Player with default balance and assert that added player is displayed on the Home Screen
+//         Then delete the added player and assert that deleted player is not displayed on the Home Screen
+//         */
+//        self.testAddPlayerWithDefaultBalance()
+//        homeScreen.deletePlayerWith(name: testPlayerName1, balance: defaultPlayerBalance, currency: defaultCurrency)
+//
+//        XCTAssertFalse(homeScreen.isPlayerDisplayed(name: testPlayerName1, balance: defaultPlayerBalance, currency: defaultCurrency))
+//    }
+//
+//    func testAddTwoPlayerWithCustomBalance() {
+//        /**
+//         Add two new Players with custom balance and assert that added players are displayed on the Home Screen
+//         */
+//        homeScreen.addPlayer()
+//        addPlayerScreen.setPlayerNameWith(testPlayerName1)
+//        addPlayerScreen.setPlayerBalanceWith(testPlayerBalance1)
+//        addPlayerScreen.addPlayer()
+//
+//        homeScreen.addPlayer()
+//        addPlayerScreen.setPlayerNameWith(testPlayerName2)
+//        addPlayerScreen.setPlayerBalanceWith(testPlayerBalance2)
+//        addPlayerScreen.addPlayer()
+//
+//        XCTAssertTrue(homeScreen.isPlayerDisplayed(name: testPlayerName1, balance: testPlayerBalance1, currency: defaultCurrency))
+//        XCTAssertTrue(homeScreen.isPlayerDisplayed(name: testPlayerName2, balance: testPlayerBalance2, currency: defaultCurrency))
+//    }
+//
+//    func testAddPlayerWithCustomBalanceThenRename() {
+//        /**
+//         Add new Player with custom balance and rename then assert that modified player is displayed on the Home Screen
+//         */
+//        homeScreen.addPlayer()
+//        addPlayerScreen.setPlayerNameWith(testPlayerName1)
+//        addPlayerScreen.setPlayerBalanceWith(testPlayerBalance1)
+//        addPlayerScreen.addPlayer()
+//        homeScreen.renamePlayerNameWith(name: testPlayerName1, balance: testPlayerBalance1, currency: defaultCurrency, newName: testPlayerName2)
+//
+//        XCTAssertTrue(homeScreen.isPlayerDisplayed(name: testPlayerName2, balance: testPlayerBalance1, currency: defaultCurrency))
+//    }
+
     func testAddPlayerWithDefaultBalanceThenAddQuickAddAmount() {
         /**
          Add new Player with default balance and add quick add amountthen assert that that modified player is displayed on the Home Screen
